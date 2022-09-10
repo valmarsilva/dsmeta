addaeee8fc1cc9af41df6458c92e793fb4ebe630
valmar Silva <valmar-ssilva@hotmail.com>
2022-09-10 19:02:15 -0300

    First  component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
-  return <h1>olá mundo</h1>;
+  return (
+    <>
+      <h1>olá mundo</h1>
+      <p> ola galera como estaão</p>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  );
 }
 
 export default App;
