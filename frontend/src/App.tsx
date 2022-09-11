195ebdc5e387be3a506f31f1ea0d68cb9289c56b
valmar Silva <valmar-ssilva@hotmail.com>
2022-09-11 12:45:50 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import NotificationButton from "./components/NotificationButton";
+
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <h1>olá mundo</h1>
-      <p> ola galera como estaão</p>
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
-      <NotificationButton />
+      <Header/>
+       <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+<SalesCard/>
+
+        </div>
+      </section>
+      </main>
     </>
   );
 }
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,10 @@
-
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
-
+import icon from "../../assets/img/notification-icon.svg";
+import "./styles.css";
 
 function NotificationButton() {
   return (
     <div className="dsmeta-red-btn">
-      <img src={icon} alt="Notificar este" />
+      <img src={icon} alt="DSMeta" />
     </div>
   );
 }
