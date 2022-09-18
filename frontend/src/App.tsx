edae94c856615e2c07216835ff085eb08772c1fe
valmar Silva <valmar-ssilva@hotmail.com>
2022-09-18 12:24:47 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
-
-import Header from "./components/Header"
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <Header/>
-       <main>
-      <section id="sales">
-        <div className="dsmeta-container">
-<SalesCard/>
-
-        </div>
-      </section>
+    <ToastContainer/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
       </main>
     </>
   );
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
 import "./styles.css";
 import {BASE_URL} from '../../utils/request';
+import { toast } from "react-toastify";
 
 type Props ={
   saleId:number;
@@ -11,7 +12,8 @@ function handleClick(id: number){
 
   axios(`${BASE_URL}/sales/${id}/notification`)
   .then(response =>{
-    console.log("sucesso")
+    toast.info("SMS enviado com sucesso");
+    
   })
 }
 function NotificationButton({saleId} :Props) {
